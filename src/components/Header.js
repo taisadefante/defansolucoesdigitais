ce290630e0a01abb092f402a98a9d55110af6fc6
tais defante <taisadefante@hotmail.com>
2025-02-13 19:24:45 -0300

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand href="/">
           <img src="/assets/defan.png" width={100} height={40} alt="Logo" />
@@ -12,8 +12,9 @@ const Header = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#about">Sobre</Nav.Link>
-            <Nav.Link href="#portfolio">Portfólio</Nav.Link>
             <Nav.Link href="#services">Serviços</Nav.Link>
+            <Nav.Link href="#portfolio">Portfólio</Nav.Link>
+            <Nav.Link href="#faq">FAQ</Nav.Link>
             <Nav.Link href="#contact">Contato</Nav.Link>
           </Nav>
         </Navbar.Collapse>
